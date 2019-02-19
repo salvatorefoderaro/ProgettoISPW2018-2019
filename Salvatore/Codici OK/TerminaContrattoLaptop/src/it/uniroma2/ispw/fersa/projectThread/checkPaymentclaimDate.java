d76e369b6570ee500fd0d1f905354322f4db7fb1
Salvatore <salvatore.foderaro@gmail.com>
2019-02-19 09:00:33 +0100

    Fatto!

---
+++ Salvatore/Codici OK/TerminaContrattoLaptop/src/it/uniroma2/ispw/fersa/projectThread/checkPaymentclaimDate.java
@@ -0,0 +1,34 @@
+package it.uniroma2.ispw.fersa.projectThread;
+
+import it.uniroma2.ispw.fersa.DAO.paymentClaimJDBC;
+
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class checkPaymentclaimDate implements Runnable {
+
+    public void checkPaymentClaimDate(){ }
+
+    @Override
+    public void run() {
+
+        while(true){
+
+            try {
+                System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + " | Checking Payment Claim date...");
+                paymentClaimJDBC.getInstance().checkPaymentClaimDate();
+            } catch (it.uniroma2.ispw.fersa.Exceptions.dbConfigMissing dbConfigMissing) {
+                System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + " | Errore nella lettura dei file di configurazione del Db");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + " | Errore nella comunicazione con il database");
+            }
+            try {
+                Thread.sleep(60000*60);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
